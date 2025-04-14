067ba0a288accb7a90c26862ab2b99ad86aa1b24
Sholana1 <shodunkedavid16@gmail.com>
2025-04-14 15:03:32 +0100

    Create discord session

--- src/components/mock-discord-ui.tsx
+++ src/components/mock-discord-ui.tsx
@@ -1,6 +1,7 @@
 import { PropsWithChildren } from "react";
 import { Icons } from "./icons";
-import { PlusCircleIcon, UserCircle } from "lucide-react";
+import { Cog, Headphones, HelpCircle, Inbox, Menu, Mic, Phone, Pin, PlusCircleIcon, Search, UserCircle, Video } from "lucide-react";
+import Image from "next/image";
 
 export const MockDiscordUI = ({children}: PropsWithChildren) => {
     return <div className="flex min-h-[800px] w-full max-w-[1200px] bg-[#202225] text-white rounded-lg overflow-hidden shadow-lg">
@@ -34,9 +35,60 @@ export const MockDiscordUI = ({children}: PropsWithChildren) => {
                         <span className="font-medium text-sm">Friends</span>
                     </div>
                     <div className="flex items-center text-sm px-2 py-1.5 rounded hover:bg-[#393c43] text-[#dcddde] cursor-not-allowed">
-                        <UserCircle className="mr-4 size-8 text-[#b9bbbe]"/>
+                        <Inbox className="mr-4 size-8 text-[#b9bbbe]"/>
                         <span className="font-medium text-sm">Nitro</span>
                     </div>
+                </div>
+                <div className="px-2 mb-4 ">
+                    <h3 className="text-sm font-semibold text-[#8e9297] px-2 mb-2 uppercase">Direct Messages</h3>
+                    <div className="flex items-center px-2 py-15 rounded bg-[#393c43] text-white cursor-pointer">
+                        <Image src="/brand-asset-profile-picture.png" alt="PingPanda Avater" width={32} height={32} className="object-cover mr-2 rounded-full"/>
+                        <span className="font-medium">PingPanda</span>
+                    </div>
+                    <div className="my-1 space-y-1">
+                        {[...Array(4)].map((_, i) => (
+                            <div key={i} className="flex items-center px-2 py-1.5 rounded text-gray-600 cursor-not-allowed">
+                                <div className="size-8 rounded-full bg-discord-background mr-3"/>
+                                <span className="font-medium">User {i + 1} </span>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+            <div className="p-2 bg-[#292b2f] flex items-center">
+                <div className="size-8 rounded-full bg-brand-700 mr-2"/>
+                <div className="flex-1">
+                        <p className="text-sm font-medium text-white">You</p>
+                        <p className="text-xs text-[#b9bbbe] flex items-center">@your_account</p>
+                </div>
+                <div className="flex items-center space-x-2">
+                    <Mic className="size-5 text-[#b9bbbe] hover:text-white cursor-pointer"/>
+                    <Headphones className="size-5 text-[#b9bbbe] hover:text-white cursor-pointer"/>
+                    <Cog className="size-5 text-[#b9bbbe] hover:text-white cursor-pointer"/>
+                </div>
+            </div>
+        </div>
+        {/* main content */}
+        <div className="flex-1 flex flex-col">
+            <div className="h-16 bg-[#36393f] flex items-center px-4 shadow-sm border-b border-[#202225]">
+                <div className="md:hidden mr4">
+                    <Menu className="size-6 text-[#b9bbbe] hover:text-white cursor-pointer"/>
+                </div>
+                <div className="flex items-center">
+                    <div className="relative">
+                        <Image src="/brand-asset-profile-picture.png" alt="PingPand discord" width={40} height={40} className="object-cover rounded-full mr-3"/>
+                        <div className="absolute bottom-0 right-3 size-3 bg-green-500 rounded-full border-2 border-[#36393f]"/> 
+                    </div>
+                    <p className="font-semibold text-white">PingPanda</p>
+                </div>
+                <div className="ml-auto flex items-center space-x-4 text-[#b9bbbe]">
+                    <Phone className="size-5 hover:text-white cursor-not-allowed hidden sm:block"/>
+                    <Video className="size-5 hover:text-white cursor-not-allowed hidden sm:block"/>
+                    <Pin className="size-5 hover:text-white cursor-not-allowed hidden sm:block"/>
+                    <UserCircle className="size-5 hover:text-white cursor-not-allowed hidden sm:block"/>
+                    <Search className="size-5 hover:text-white cursor-not-allowed hidden sm:block"/>
+                    <Inbox className="size-5 hover:text-white cursor-not-allowed hidden sm:block"/>
+                    <HelpCircle className="size-5 hover:text-white cursor-not-allowed hidden sm:block"/>
                 </div>
             </div>
         </div>
